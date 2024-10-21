761c174220d79bb83f02c50a7d04080edf536ba1
nfzmhn <zidninurfauzi2203@gmail.com>
2024-10-21 17:04:22 +0700

    First Commit (Update view)

---
+++ app/src/main/java/pam/lat/hellotoast/NameViewModel.kt
@@ -0,0 +1,10 @@
+package pam.lat.hellotoast
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NameViewModel : ViewModel() {
+    val currentName: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+}
